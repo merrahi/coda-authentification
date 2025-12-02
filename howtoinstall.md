713b59ff81de8deff9d553406ccad1b923a9ebc2
lilianPLV <lilianpulv@gmail.com>
2025-12-02 15:01:50 +0100

    create register file and bootstrap

--- howtoinstall.md
+++ howtoinstall.md
@@ -1,1 +1,3 @@
 **Coda Authentification Project**
+1.   git pull origin master
+2.   composer install
---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <script src="/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <form>
+    <div class="form-group">
+        <label for="exampleInputUsername">Username</label>
+        <input type="text" class="form-control" name="username" id="exampleInputUsername1" aria-describedby="UsernameHelp" placeholder="Enter Username">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+</html>
+
+
